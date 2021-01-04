43413ac7e0695a44296c0b3a7882563d88e905fe
Brian Emilius <be@rts.dk>
2021-01-04 13:38:38 +0100

    add first component

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "react-dom";
+
+function App() {
+	return (
+		<h1>Hej fra React</h1>
+	);
+};
+
+render(<App/>, document.getElementById("root"));
